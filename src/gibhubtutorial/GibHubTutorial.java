488028346a5c4d64473fdc3afcd188f918ac6a5a
GaneshaAK <111953703+GaneshaAK@users.noreply.github.com>
2023-09-01 16:54:10 +0530

    Update GibHubTutorial.java

--- src/gibhubtutorial/GibHubTutorial.java
+++ src/gibhubtutorial/GibHubTutorial.java
@@ -22,9 +22,9 @@ public class GibHubTutorial {
         
         
         System.out.println("Test");
+    }
 
-        public void x() {
+    public void x() {
         int x = 2+6;
-        }
     }
 }
