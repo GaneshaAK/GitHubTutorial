f42d6a85686a0fac011497f7796c3cd6b8f7fdc4
AKGanesha <ganesh@voxelgrids.com>
2023-09-04 11:23:24 +0530

    Sample

--- src/gibhubtutorial/GibHubTutorial.java
+++ src/gibhubtutorial/GibHubTutorial.java
@@ -14,17 +14,7 @@ public class GibHubTutorial {
      * @param args the command line arguments
      */
     
-    public void x() {
-        int x = 2+6;
-    }
-    
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Extra statement");
-        System.out.println("Extra 3rd Statement");
-        System.out.println("Extra 2nd Statement");
-        
-        
-        System.out.println("Test");
     }
 }
